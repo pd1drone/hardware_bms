acbc103156d3ee7a2201ceed5631d6de0bad3701
Karl Angelo Lim <55650048+pd1drone@users.noreply.github.com>
2024-10-23 15:04:37 +0800

    Update BMSNodemcu.ino

--- BMSNodemcu.ino
+++ BMSNodemcu.ino
@@ -1,57 +1,232 @@
+// Libraries
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiClient.h>
+
 
 #include <FirebaseArduino.h>
 #include <ESP8266WiFi.h>
 
-#define FIREBASE_AUTH "Ug0VVnHLdMDfZeLWn7GI5rbjBMSvsmzdSy3YmqvB" //Your Firebase Web API Key
-#define FIREBASE_HOST "iot-bms-f7f32-default-rtdb.asia-southeast1.firebasedatabase.app" //Your Firebase URL
-#define WIFI_SSID "GlobeAtHome_B6DC0_5"     //Your WIFI SSID
-#define WIFI_PASSWORD "ua4Z6wEY" //Your WIFI Password
+#define FIREBASE_AUTH "Ug0VVnHLdMDfZeLWn7GI5rbjBMSvsmzdSy3YmaqvB" //Your Firebase Web API Key
+#define FIREBASE_HOST "iot-bms-f7f-default-rtdb.asia-southeast1.firebasedatabase.app" //Your Firebase URL
+
 
 bool RoomLEDStates[10] = {false, false, false, false, false, false, false, false, false, false};
 
+
+// Hostname
+#define HOSTNAME "BMS"
+
+// WiFi network created by ESP8266
+const char* ssid = "BMS";
+const char* password = "admin123";  // Add your password here
+
+// Create server
+WiFiServer server(80);
+
+// Multicast DNS responder
+MDNSResponder mdns;
+
+bool Connected = false;
+
+// Functions declaration
+void connect_network(String wifiName, String wifiPassword);
+
 void setup() {
-  // Start Serial communication
+
+  // Start Serial port
   Serial.begin(9600);
+  delay(10);
 
-  // Connect to Wi-Fi
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  // Create WiFi network
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);  // Use the password here
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+  Serial.println(WiFi.softAPIP());
+
+  // Start mDNS
+  if (!MDNS.begin(HOSTNAME)) {
+    Serial.println("Error setting up MDNS responder!");
+    while(1) {
+      delay(1000);
+    }
   }
-  Serial.println();
-  Serial.println("Connected to Wi-Fi");
-  Serial.println(WiFi.localIP());
+  Serial.println("mDNS responder started");
 
-  // Connect to Firebase
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  // Add service to MDNS-SD
+  MDNS.addService("http", "tcp", 80);
+
+  // Look for networks
+  int n = WiFi.scanNetworks();
+  Serial.println("Scan done");
+  if (n == 0)
+    Serial.println("No networks found !! ");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" Networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*");
+      delay(10);
+    }
+  }
+  Serial.println("");
 }
 
 void loop() {
-  // Get boolean value from Firebase and print it
-  for (int i = 0; i < 10; i++) {
-      String roomKey = "Room" + String(i + 1);
-      RoomLEDStates[i] = Firebase.getBool(roomKey);
+
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  Serial.println("");
+  Serial.println("New client");
+
+  // Wait for data from client to become available
+  while(client.connected() && !client.available()){
+    delay(1);
   }
 
-  sendJSONResponse();
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  //Serial.println(req);
+  client.flush();
 
-  if (Serial.available() > 0) {
-    String command = Serial.readStringUntil('\n');
-    command.trim();
-    
-    if (command == "all on") {
-      setAllLEDs(true); // Turn all LEDs on
-    } else if (command == "all off") {
-      setAllLEDs(false); // Turn all LEDs off
-    } else {
-      handleSerialCommand(command); // Handle other commands
+  // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+  s += "<html><head>";
+  s += "</head>";
+  s += "<body><div class=\"container\">";
+
+  // Match the request
+  if (req.indexOf("/login") != -1){
+
+    Serial.println("Connecting to local network");
+
+    // Parse response
+    String wifiNameParameter = "wifiName=";
+    String wifiPasswordParameter = "wifiPassword=";
+    int indexName = req.indexOf(wifiNameParameter);
+    int indexPassword = req.indexOf(wifiPasswordParameter);
+    int indexBoard = req.indexOf("board=");
+
+    String wifiName = req.substring(indexName + wifiNameParameter.length(), indexPassword - 1);
+    String wifiPassword = req.substring(indexPassword + wifiPasswordParameter.length(), indexBoard - 1);
+
+    Serial.print("WiFi name: ");
+    Serial.println(wifiName);
+    Serial.print("WiFi password: ");
+    Serial.println(wifiPassword);
+
+    // Build answer
+    s += "<div>Logged in!</div>";
+    s += "<div>Now please connect to the WiFi with internet to control BMS </div>";
+    s += "</div></body></html>\r\n";
+
+    // Send the response to the client
+    client.print(s);
+    //delay(100);
+    client.stop();
+    Serial.println("Client disonnected");
+
+    // Connect to other network
+    connect_network(wifiName, wifiPassword);
+
+    // Do other things
+    // Your code goes here
+
+  }
+  else {
+
+    Serial.println("Serving interface");
+
+    s += "<div>WiFi name: <input type='text' id='wname'></div>";
+    s += "<div>WiFi password: <input type='text' id='pass'></div>";
+    s += "<div><input class=\"btn\" type=\"button\" value=\"Validate\" onclick=\"login()\"></div>";
+    s += "</div>";
+
+    s += "<script>function login() {";
+    s += "var wifiName = document.getElementById('wname').value;";
+    s += "var wifiPassword = document.getElementById('pass').value;";
+    s += "window.location.href = '/login?wifiName=' + wifiName + '&wifiPassword=' + wifiPassword + '&board=';}";
+    s += "</script>";
+    s += "</body></html>\r\n";
+
+    // Send the response to the client
+    client.print(s);
+    //delay(100);
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+
+
+
+  while (Connected){
+      for (int i = 0; i < 10; i++) {
+      String roomKey = "Room" + String(i + 1);
+      RoomLEDStates[i] = Firebase.getBool(roomKey);
+    }
+
+    sendJSONResponse();
+
+    if (Serial.available() > 0) {
+      String command = Serial.readStringUntil('\n');
+      command.trim();
+      
+      if (command == "all on") {
+        setAllLEDs(true); // Turn all LEDs on
+      } else if (command == "all off") {
+        setAllLEDs(false); // Turn all LEDs off
+      } else {
+        handleSerialCommand(command); // Handle other commands
+      }
     }
   }
 
 }
 
+void connect_network(String wifiName, String wifiPassword) {
+
+  // Connect to other network
+  WiFi.mode(WIFI_STA);
+
+  const char* other_ssid = wifiName.c_str();
+  const char* other_password = wifiPassword.c_str();
+
+  WiFi.begin(other_ssid, other_password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(other_ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Connected = true;
+}
 
 void handleSerialCommand(String command) {
   int roomNumber = command.substring(4, command.indexOf(' ')).toInt();

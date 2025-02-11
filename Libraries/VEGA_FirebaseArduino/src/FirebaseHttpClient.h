ead884a8b8cac0947415c6bde19f71cb64d2256b
Karl Angelo Lim <55650048+pd1drone@users.noreply.github.com>
2025-02-11 12:38:20 +0800

    Add files via upload

---
+++ Libraries/VEGA_FirebaseArduino/src/FirebaseHttpClient.h
@@ -0,0 +1,47 @@
+#ifndef FIREBASE_HTTP_CLIENT_H
+#define FIREBASE_HTTP_CLIENT_H
+
+#include <string>
+
+#include "Arduino.h"
+#include "Stream.h"
+
+struct HttpStatus
+{
+  static const int TEMPORARY_REDIRECT = 307;
+};
+
+class FirebaseHttpClient
+{
+public:
+  static FirebaseHttpClient *create();
+
+  virtual void setReuseConnection(bool reuse) = 0;
+  virtual void begin(const std::string &url) = 0;
+  virtual void begin(const std::string &host, const std::string &path) = 0;
+
+  virtual void end() = 0;
+
+  virtual void addHeader(const std::string &name, const std::string &value) = 0;
+  virtual void collectHeaders(const char *header_keys[],
+                              const int header_key_count) = 0;
+  virtual std::string header(const std::string &name) = 0;
+
+  virtual int sendRequest(const std::string &method, const std::string &data) = 0;
+
+  virtual std::string getString() = 0;
+
+  virtual Stream *getStreamPtr() = 0;
+
+  virtual std::string errorToString(int error_code) = 0;
+
+  virtual bool connected() = 0;
+
+protected:
+  static const uint16_t kFirebasePort = 443;
+};
+
+static const char kFirebaseFingerprint[] =
+    "47:45:4A:A1:BD:32:91:C4:52:C9:A6:4B:44:54:26:0B:8B:09:50:9C"; // 2025-01
+
+#endif // FIREBASE_HTTP_CLIENT_H
